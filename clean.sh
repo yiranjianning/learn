fe6230aa2aa11cef6ab61f9c57a73964a4bf9656
smle <jianning1991@126.com>
2017-12-29 07:32:39 +0800

    add all file

---
+++ clean.sh
@@ -0,0 +1,16 @@
+find . -name "*.aux"  | xargs rm -f
+find . -name "*.bbl"  | xargs rm -f
+find . -name "*.bak"  | xargs rm -f
+find . -name "*.blg"  | xargs rm -f
+find . -name "*.out"  | xargs rm -f
+find . -name "*.thm"  | xargs rm -f
+find . -name "*.toc"  | xargs rm -f
+find . -name "*.synctex*"  | xargs rm -f
+find . -name "*.glg"  | xargs rm -f
+find . -name "*.glo"  | xargs rm -f
+find . -name "*.gls"  | xargs rm -f
+find . -name "*.ist"  | xargs rm -f
+find . -name "*.idx"  | xargs rm -f
+find . -name "*.ilg"  | xargs rm -f
+find . -name "*.ind"  | xargs rm -f
+find . -name "logo.*"  | xargs rm -f
